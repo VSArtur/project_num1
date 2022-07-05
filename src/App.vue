cb2edae69ca57b5a72543688f99e27a7b2341fa7
VSArtur <99079377+VSArtur@users.noreply.github.com>
2022-07-05 16:20:31 +0300

    new sait

---
+++ src/App.vue
@@ -0,0 +1,607 @@
+<template>
+<div class="wrapper">
+  <header>
+ <div class="header">
+
+<div class="header__menu">
+ <a href="#"><img src="../public/img/Hamburger_LG.svg" alt="-"></a> 
+ <span class="header__text__menu">Меню</span>
+
+</div>
+ 
+
+
+
+
+
+
+<div class="header__dropdown">
+  <button class="header__dropbtn">Имя Отчество</button>
+  <div class="header__dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+</div>
+    
+
+
+  </div>
+  
+  </header>
+
+
+<div class="header__block">
+<button type="button" class="button__first">Добавить достижение</button>
+<a href="#"><img src="../public/img/icon.svg" alt=""></a>
+</div>
+
+
+
+  
+
+
+
+ <section>
+
+  
+<div class="main__section">
+
+
+  <div class="main__section__button">
+<button type="button" class="button__first">
+Результаты учебной деятельности
+  </button>
+   <button type="button" class="button__first">
+Олимпиады
+  </button>
+   <button type="button" class="button__first">
+Иследовательская деятельность
+  </button>
+   <button type="button" class="button__first">
+Спортивные достижения (в ОО)
+  </button>
+   <button type="button" class="button__first">
+Спортивные достижения (вне ОО)
+  </button>
+   <button type="button" class="button__first">
+Дополнительное образование (в ОО)
+  </button>
+   <button type="button" class="button__first">
+Дополнительное образование (вне ОО)
+  </button>
+   <button type="button" class="button__first">
+Участие в мероприятиях (в ОО)
+  </button>
+   <button type="button" class="button__first">
+Участие в мероприятиях (вне ОО)
+  </button>
+   <button type="button" class="button__first">
+Творчество
+  </button>
+   <button type="button" class="button__first">
+Прочие олимпиады
+  </button>
+   <button type="button" class="button__first">
+Экскурсии
+  </button>
+   <button type="button" class="button__first">
+Элективные курсы
+  </button>
+  </div>
+  
+<div>
+
+<table class="sxema">
+	<tbody>
+		<tr class="blue__line">
+      
+			<td>-</td>
+			<td>4,34</td>
+			<td>-</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+      
+			<td></td>
+			<td></td>
+			<td>-</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr  class="blue__line">
+      
+			<td></td>
+			<td></td>
+			<td>-</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td>5,45</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+      
+			<td></td>
+			<td></td>
+			<td>6,73</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td>-</td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr class="blue__line">
+      
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+      
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr class="blue__line">
+      
+			<td></td>
+			<td></td>
+			<td>-</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td>8,92</td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+      
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td>9,93</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr class="blue__line">
+      
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td>-</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+
+			<td>7,33</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td>9,93</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr class="blue__line">
+			<td></td>
+			<td></td>
+			<td></td>
+			<td>-</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td>4,22</td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+      
+			<td></td>
+			<td></td>
+			<td>-</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td>-</td>
+		</tr>
+	</tbody>
+</table>
+</div>
+</div>
+
+<div class="main__footer">
+   <button class="button__first" type="button">Предоставить доступ</button>
+   <span class="main__footer__total">105,35</span>
+</div>
+
+
+
+
+
+  
+ </section>
+</div>
+
+</template>
+<script>
+// export default {
+//   data:{
+//     return:{
+
+//     }
+//   },
+// }
+</script>
+
+<style>
+/* #app {
+
+} */
+*{
+	padding: 0;
+	margin: 0;
+	border: 0;
+}
+*,*:before,*:after{
+	-moz-box-sizing: border-box;
+	-webkit-box-sizing: border-box;
+	box-sizing: border-box;
+}
+:focus,:active{outline: none;}
+a:focus,a:active{outline: none;}
+
+nav,footer,header,aside{display: block;}
+
+html,body{
+	height: 100%;
+	width: 100%;
+	font-size: 100%;
+	line-height: 1;
+	font-size: 14px;
+	-ms-text-size-adjust: 100%;
+	-moz-text-size-adjust: 100%;
+	-webkit-text-size-adjust: 100%;
+}
+input,button,textarea{font-family:inherit;}
+
+input::-ms-clear{display: none;}
+button{cursor: pointer;}
+button::-moz-focus-inner {padding:0;border:0;}
+a, a:visited{text-decoration: none;}
+a:hover{text-decoration: none;}
+ul li{list-style: none;}
+img{vertical-align: top;}
+
+h1,h2,h3,h4,h5,h6{font-size:inherit;font-weight: 400;}
+/* -------------------- */
+
+body{
+
+font-family: 'Akshar', sans-serif;
+font-family: 'Montserrat', sans-serif;
+font-family: 'Poppins', sans-serif;
+}
+
+.wrapper{
+  max-width: 1850px;
+  margin: 0 auto;
+
+}
+
+
+.header{
+  min-height: 80px;
+  padding: 32px 35px 32px 35px;
+  background: linear-gradient(91.2deg, #0081CA -3.93%, #24B0FF 85.84%);
+  display: flex;
+  justify-content: space-between;
+  color:white;
+  align-items: center;
+
+}
+
+
+.header__menu{
+  display: flex;
+}
+.header__menu__name{
+font-weight: 500;
+font-size: 16px;
+text-decoration: none;
+color: #fff;
+line-height: 20px;
+margin-top: 10px;
+}
+
+
+.header__text__menu{
+display: flex;
+margin-top: 12px;
+margin-left: 10px;
+}
+
+.main__section{
+  display: flex;
+  margin: 65px 20px ;
+
+}
+
+.main__section__button{
+  display: flex;
+    flex-direction: column;
+}
+
+.button__first{
+  background: linear-gradient(91.2deg, #0081CA -3.93%, #24B0FF 85.84%);
+border-radius: 10px;
+font-weight: 700;
+font-size: 14px;
+line-height: 17px;
+padding: 16px 42px 16px 42px;
+margin-bottom: 10px;
+margin-right: 16px;
+color:#ffff
+
+}
+table.sxema{
+	width: 1100px;
+	border-collapse:collapse;
+	border-spacing:0;
+	height: 950px;
+}
+table.sxema,table.sxema td, table.sxema th {
+
+}
+table.sxema,table.sxema td{
+ border: 1px solid #A7A7A7;
+}
+table.sxema td,table.sxema th {
+	padding: 3px;
+	width: 30px;
+	height: 35px;
+color:#3E3E3E;
+font-weight: 700;
+font-size: 16px;
+line-height: 19.5px;
+text-align: center;
+
+ 
+}
+table.sxema th {
+	background: #347c99; 
+	color: #fff; 
+	font-weight: normal;
+
+}
+.blue__line{
+  background: #CBECFF;
+
+
+}
+
+.header__block{
+  display: flex;
+  justify-content: end;
+  margin-top:50px;
+  align-items: center;
+  margin-right: 20px;
+  
+}
+
+.header__block>a{
+  padding-bottom: 11px;
+}
+
+
+.header__dropbtn {
+    background-color: #4CAF50;
+    color: white;
+    padding: 16px;
+    font-size: 16px;
+    border: none;
+}
+
+.header__dropbtn::after{
+
+}
+.header__drob__btn::after{
+background: url('../public/img/Vector 2.svg') left no-repeat;
+width: 40px;
+height: 40px;
+}
+
+
+.header__dropdown {
+    position: relative;
+    display: inline-block;
+}
+
+
+.header__dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f1f1f1;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+}
+
+
+.header__dropdown-content a {
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+}
+.header__dropdown-content a:hover {background-color: #ddd;}
+
+
+.header__dropdown:hover .header__dropdown-content {display: block;}
+
+.header__dropdown .header__dropbtn {background: #24B0FF}
+
+
+.main__footer{
+  display: flex;
+justify-content: end;
+align-items: center;
+margin-right: 25px;
+margin-top: 32px;
+
+}
+
+.main__footer__total{
+  font-weight: 700;
+font-size: 20px;
+line-height: 24px;
+margin-bottom: 14px;
+color: #0081CA;
+}
+
+
+@media (max-width:1360px) {
+.sxema{
+max-width: 900px;
+max-height: 800px;  
+}
+}
+
+@media (max-width:1278px) {
+.sxema{
+max-width: 600px;
+max-height: 600px;  
+}
+}
+@media (max-width:841px) {
+.sxema{
+max-width: 400px;
+max-height: 400px;  
+}
+}
+
+@media (max-width:636px) {
+.sxema{
+max-width: 300px;
+max-height: 300px;  
+}
+}
+@media (max-width:636px) {
+.main__section{
+max-width: 300px;
+
+display: flex;
+flex-direction: column;
+margin-left: 160px;
+
+}
+} 
+
+@media (max-width:536px) {
+.main__section{
+max-width: 300px;
+
+display: flex;
+flex-direction: column;
+margin-left: 40px;
+
+}
+}
+
+
+ @media (max-width:553px) {
+.main__section{
+max-width: 300px;
+margin: 0 auto;
+margin-top: 30px;
+display: flex;
+flex-direction: column;
+
+
+}
+} 
+
+
+
+
+</style>
